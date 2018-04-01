4b94c20dfa8c78f963ecb56bbce5e59109d32fb1
RSHAW <scrudgey@gmail.com>
2018-03-31 20:01:15 -0700

    changed name

--- galaxy.py
+++ galaxy.py
@@ -3,23 +3,32 @@
 ASCII Galaxy
 Generate and post ASCII images of galaxies.
 """
-import cred
+import os
+import random
 import twitter
-import pickle
+
+import cred
+import ascii
 
 api = twitter.Api(consumer_key=cred.consumer_key,
                   consumer_secret=cred.consumer_secret,
                   access_token_key=cred.access_token,
                   access_token_secret=cred.access_token_secret)
+# print(api.VerifyCredentials())
+
+def test():
+  print generate_image()
 
 def post_ascii_galaxy():
   """Generate and then post an ASCII galaxy."""
-  # print(api.VerifyCredentials())
   img = generate_image()
   post_image(img)
 
 def generate_image():
-  return pickle.load(open("img.p", "rb"))
+  # TODO: get random image from folder
+  path =  'imgs/' + random.choice(os.listdir('imgs'))
+  img = ascii.handle_image_conversion(path)
+  return img
 
 def post_image(img):
   status = api.PostUpdate(img)
