22e48e58368dca2c9a14c055aed7f1551fa70912
RSHAW <scrudgey@gmail.com>
2018-03-31 21:55:41 -0700

    working version with test functions

--- galaxy.py
+++ galaxy.py
@@ -9,12 +9,22 @@ import twitter
 
 import cred
 import ascii
+import wiki_scrape
 
 api = twitter.Api(consumer_key=cred.consumer_key,
                   consumer_secret=cred.consumer_secret,
                   access_token_key=cred.access_token,
                   access_token_secret=cred.access_token_secret)
 # print(api.VerifyCredentials())
+
+def test_file(name):
+  image_names = os.listdir('imgs')
+  for image_name in image_names:
+    if name in image_name:
+      path =  'imgs/' + image_name
+      img = ascii.handle_image_conversion(path)
+      print path
+      print img
 
 def test():
   print generate_image()
@@ -25,7 +35,6 @@ def post_ascii_galaxy():
   post_image(img)
 
 def generate_image():
-  # TODO: get random image from folder
   path =  'imgs/' + random.choice(os.listdir('imgs'))
   img = ascii.handle_image_conversion(path)
   return img
@@ -35,4 +44,5 @@ def post_image(img):
   print(status.text)
 
 if __name__=='__main__':
+  print 'posting galaxy...'
   post_ascii_galaxy()
